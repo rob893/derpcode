808a9dc01a08d6bbe1d058dd2e915791aab3a0ee
Robert Herber <rwherber@gmail.com>
2025-05-25 01:43:06 -0400

    ci: fix ci

--- derpcode-ui/src/main.tsx
+++ derpcode-ui/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router';
+import { HashRouter as Router } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
