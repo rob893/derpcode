f8de74d827fe1f8f3d07473425451cee9233a209
Robert Herber <rwherber@gmail.com>
2025-08-31 02:03:22 -0400

    chore: trigger deploy

--- DerpCode.API/Constants/CacheKeys.cs
+++ DerpCode.API/Constants/CacheKeys.cs
@@ -2,5 +2,8 @@ namespace DerpCode.API.Constants;
 
 public static class CacheKeys
 {
+    /// <summary>
+    /// Key for caching the list of problems.
+    /// </summary>
     public const string Problems = "Problems";
 }
